e9dde68435667462cc859d0ec3b26bcbd04d6cd7
dahrif <dahri.fatima.dev@gmail.com>
2021-06-21 12:50:48 +0100

    evenement

---
+++ 4-composants-et props/to-do-list-v1/app.js
@@ -0,0 +1,36 @@
+class Title extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        
+    }
+}
+
+class FormAdd extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render()
+}
+
+class Task extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render()
+}
+
+class TaskList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render()
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render()
+}
